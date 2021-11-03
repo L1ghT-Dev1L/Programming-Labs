1e64272741811e9e9b731a43726ace294a67a554
L1ghT-Dev1L <66724550+L1ghT-Dev1L@users.noreply.github.com>
2021-11-03 10:26:59 +0300

    Create Main.java

---
+++ Lab №2/Main.java
@@ -0,0 +1,24 @@
+import pokemons.*;
+import ru.ifmo.se.pokemon.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Battle b = new Battle();
+        Pokemon p1 = new Eevee("Олег", 1);
+        Pokemon p2 = new HoopaConfined("Иван", 1);
+        Pokemon p3 = new Leafeon("Семён", 1);
+        Pokemon p4 = new Zubat("Саша", 1);
+        Pokemon p5 = new Golbat("Егор", 1);
+        Pokemon p6 = new Crobat("Никита", 1);
+        b.addAlly(p1);
+        b.addAlly(p2);
+        b.addAlly(p3);
+        b.addFoe(p4);
+        b.addFoe(p5);
+        b.addFoe(p6);
+
+        b.go();
+    }
+
+}
