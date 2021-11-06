5f7266532dd4ffbdab199073503bf47d8946e3e0
L1ghT-Dev1L <66724550+L1ghT-Dev1L@users.noreply.github.com>
2021-11-06 13:41:25 +0300

    Update Blizzard.java

--- Lab №2/moves/Blizzard.java
+++ Lab №2/moves/Blizzard.java
@@ -1,12 +1,12 @@
 package moves;
 import ru.ifmo.se.pokemon.*;
 
-public class Blizzard extends SpecialMove{
+public class Blizzard extends SpecialMove {
     public Blizzard() { super(Type.ICE, 110, 0.7); }
 
     @Override
     protected void applyOppEffects(Pokemon def) {
-        if(Math.random() < 0.1){
+        if(Math.random() < 0.1) {
             Effect.freeze(def);
         }
     }
