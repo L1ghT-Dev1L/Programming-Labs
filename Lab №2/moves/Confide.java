1229444f95b5f7e25e4fa4db56f8037f775a0dc6
L1ghT-Dev1L <66724550+L1ghT-Dev1L@users.noreply.github.com>
2021-11-06 12:02:52 +0300

    Update Confide.java

--- Lab №2/moves/Confide.java
+++ Lab №2/moves/Confide.java
@@ -2,9 +2,7 @@ package moves;
 import ru.ifmo.se.pokemon.*;
 
 public class Confide extends StatusMove{
-    public Confide(){
-        super(Type.NORMAL, 0, 100);
-    }
+    public Confide(){ super(Type.NORMAL, 0, 100); }
 
     @Override
     protected void applyOppEffects(Pokemon def) { def.setMod(Stat.SPECIAL_ATTACK, -1); }
