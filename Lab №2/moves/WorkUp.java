d1f4f7c936560585f4f57805b8a56f22717457ef
L1ghT-Dev1L <66724550+L1ghT-Dev1L@users.noreply.github.com>
2021-11-06 12:04:06 +0300

    Update WorkUp.java

--- Lab №2/moves/WorkUp.java
+++ Lab №2/moves/WorkUp.java
@@ -2,9 +2,7 @@ package moves;
 import ru.ifmo.se.pokemon.*;
 
 public class WorkUp extends StatusMove {
-    public WorkUp(){
-        super(Type.NORMAL, 0, 100);
-    }
+    public WorkUp(){ super(Type.NORMAL, 0, 100); }
 
     @Override
     protected void applySelfEffects(Pokemon self){
