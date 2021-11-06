051d818c584d0ac258c57c1182512ae20af92e93
L1ghT-Dev1L <66724550+L1ghT-Dev1L@users.noreply.github.com>
2021-11-06 13:59:27 +0300

    Update Facade.java

--- Lab №2/moves/Facade.java
+++ Lab №2/moves/Facade.java
@@ -1,20 +1,24 @@
 package moves;
+
 import ru.ifmo.se.pokemon.*;
 
-public class Facade extends PhysicalMove{
-    public Facade() { super(Type.NORMAL, 70, 100); }
+public class Facade extends PhysicalMove {
+    public Facade() {
+        super(Type.NORMAL, 70, 100);
+    }
 
     @Override
     protected void applyOppDamage(Pokemon def, double damage) {
         Status def_stat = def.getCondition();
-        if(def_stat == Status.BURN || def_stat == Status.POISON || def_stat == Status.PARALYZE){
+        if (def_stat == Status.BURN || def_stat == Status.POISON || def_stat == Status.PARALYZE) {
             super.applyOppDamage(def, 2 * damage);
-        }
-        else{
+        } else {
             super.applyOppDamage(def, damage);
         }
     }
 
     @Override
-    protected String describe() { return ("Использует Facade"); }
+    protected String describe() {
+        return ("Использует Facade");
+    }
 }
