c5f842e141021321e386410d760bd7a33b5389ef
L1ghT-Dev1L <66724550+L1ghT-Dev1L@users.noreply.github.com>
2021-11-06 14:00:03 +0300

    Update Flamethrower.java

--- Lab №2/moves/Flamethrower.java
+++ Lab №2/moves/Flamethrower.java
@@ -1,19 +1,26 @@
 package moves;
+
 import ru.ifmo.se.pokemon.*;
 
-public class Flamethrower extends SpecialMove{
-    public Flamethrower() { super(Type.FIRE, 90, 100); }
+public class Flamethrower extends SpecialMove {
+    public Flamethrower() {
+        super(Type.FIRE, 90, 100);
+    }
 
     @Override
     protected void applyOppEffects(Pokemon def) {
-        if(Math.random() < 0.1){
+        if (Math.random() < 0.1) {
             Effect.burn(def);
         }
     }
 
     @Override
-    protected void applyOppDamage(Pokemon def, double damage) { super.applyOppDamage(def, damage); }
+    protected void applyOppDamage(Pokemon def, double damage) {
+        super.applyOppDamage(def, damage);
+    }
 
     @Override
-    protected String describe() { return "использует Flamethrower"; }
+    protected String describe() {
+        return "использует Flamethrower";
+    }
 }
