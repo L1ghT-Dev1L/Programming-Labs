6bc08a4da959630f493e75ab7315c3bb0fe11e67
L1ghT-Dev1L <66724550+L1ghT-Dev1L@users.noreply.github.com>
2021-11-06 13:57:23 +0300

    Update DefenseCurl.java

--- Lab №2/moves/DefenseCurl.java
+++ Lab №2/moves/DefenseCurl.java
@@ -1,14 +1,19 @@
 package moves;
+
 import ru.ifmo.se.pokemon.*;
 
-public class DefenseCurl extends StatusMove{
-    public DefenseCurl() { super(Type.NORMAL, 0, 100); }
+public class DefenseCurl extends StatusMove {
+    public DefenseCurl() {
+        super(Type.NORMAL, 0, 100);
+    }
 
     @Override
-    protected void applySelfEffects(Pokemon self){
+    protected void applySelfEffects(Pokemon self) {
         self.setMod(Stat.DEFENSE, 1);
     }
 
     @Override
-    protected String describe() { return "использует Defense Curl"; }
+    protected String describe() {
+        return "использует Defense Curl";
+    }
 }
