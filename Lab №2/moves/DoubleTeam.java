b6705f62d028c8698024027516d705de5c61202e
L1ghT-Dev1L <66724550+L1ghT-Dev1L@users.noreply.github.com>
2021-11-06 13:57:57 +0300

    Update DoubleTeam.java

--- Lab №2/moves/DoubleTeam.java
+++ Lab №2/moves/DoubleTeam.java
@@ -1,12 +1,19 @@
 package moves;
+
 import ru.ifmo.se.pokemon.*;
 
 public class DoubleTeam extends StatusMove {
-    public DoubleTeam() { super(Type.NORMAL, 0, 100); }
+    public DoubleTeam() {
+        super(Type.NORMAL, 0, 100);
+    }
 
     @Override
-    protected void applySelfEffects(Pokemon self) { self.setMod(Stat.EVASION, 1); }
+    protected void applySelfEffects(Pokemon self) {
+        self.setMod(Stat.EVASION, 1);
+    }
 
     @Override
-    protected String describe() { return "использует Double Team"; }
+    protected String describe() {
+        return "использует Double Team";
+    }
 }
