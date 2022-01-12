9152f27299c51fd71b2cf9c33492f7074c7fa33f
L1ghT-Dev1L <66724550+L1ghT-Dev1L@users.noreply.github.com>
2022-01-12 19:31:01 +0300

    Update Item.java

--- Lab №3/Abstracts/Item.java
+++ Lab №3/Abstracts/Item.java
@@ -7,7 +7,7 @@ public abstract class Item {
 
     public Item(String name, int count) {
         this.name = name;
-        if (count < 1) {
+        if (count <= 0) {
             System.out.println("Неправильно установлено количество предмета. Оно не может быть меньше 1");
             this.count = 1;
         } else {
