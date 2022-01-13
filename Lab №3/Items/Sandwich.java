441045916b57566afadf03f8759e3fa1e3812ed2
L1ghT-Dev1L <66724550+L1ghT-Dev1L@users.noreply.github.com>
2022-01-13 17:58:52 +0300

    Update Sandwich.java

--- Lab №3/Items/Sandwich.java
+++ Lab №3/Items/Sandwich.java
@@ -3,6 +3,8 @@ package ThirdLab.Items;
 import ThirdLab.Abstracts.Food;
 import ThirdLab.Properties.Ingredients;
 
+import java.util.Objects;
+
 public class Sandwich extends Food {
 
     private final Ingredients mainIngredient;
@@ -26,4 +28,17 @@ public class Sandwich extends Food {
         return String.format("просто %s", super.getName().toLowerCase());
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Sandwich other = (Sandwich) obj;
+        return super.getName().equals(other.getName()) && this.mainIngredient == other.mainIngredient;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), mainIngredient);
+    }
 }
+
